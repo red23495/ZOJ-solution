07b9e29d754eaf787900d962b2cffc4c12ec43ab
red23495 <red23495@yahoo.com>
2018-08-09 22:55:07 +0600

    Create 3610_Yet_Another_Story_of_Rock-paper-scissors.cpp

---
+++ 3610_Yet_Another_Story_of_Rock-paper-scissors.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int test;
+    string b,g,c;
+    cin >> test;
+    while(test--) {
+        cin >> b >> g >> c;
+        cout << g <<" will survive" << endl;
+    }
+    return 0;
+}
